593fbbe359dd7d280a925d2972a687b18f3073c9
Antti Humalamäki <viagra.salesman@gmail.com>
2008-01-31 14:18:56 +0000

    netbeans wut

---
+++ tetris/nbproject/Makefile-Release.mk
@@ -0,0 +1,58 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=
+CCC=
+CXX=
+FC=
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Release/None-Windows
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} dist/Release/None-Windows/tetris.exe
+
+dist/Release/None-Windows/tetris.exe: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/None-Windows
+	${LINK.c} -o dist/Release/None-Windows/tetris ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Release
+	${RM} dist/Release/None-Windows/tetris.exe
+
+# Subprojects
+.clean-subprojects:
