e27b0354eb9abd2c5f51b1be23439a018254ad33
Antti Humalamäki <viagra.salesman@gmail.com>
2008-03-02 17:58:39 +0000

    selitykset nimille

--- ESlotType.h
+++ ESlotType.h
@@ -1,16 +1,47 @@
 /**
  * ESlotType
- * Kentän matrix koostuu näistä arvoista.
+ *
+ * $Id$
+ * $HeadURL$
+ *
+ * Kentän matriisi koostuu näistä arvoista.
  */
 
 enum SLOT_TYPE
 {
     EMPTY = 0,
+    /*
+      ZZ
+       ZZ
+    */
     Z = 1,
+    /*
+       SS
+      SS
+    */
     S = 2,
+    /*
+      IIII
+    */
     I = 3,
+    /*
+      OO
+      OO
+    */
     O = 4,
+    /*
+        L
+      LLL
+    */
     L = 5,
+    /*
+      J
+      JJJ
+    */
     J = 6,
+    /*
+       T
+      TTT
+    */
     T = 7
 };
