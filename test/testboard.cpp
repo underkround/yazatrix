6737bf9be7b7371011a3f132091ccff53033794f
Jussi Rajala <zemm@iki.fi>
2008-03-04 12:26:40 +0000

    test for test =P

--- test/testboard.cpp
+++ test/testboard.cpp
@@ -1,6 +1,14 @@
 #include "../TetrisBoard.h"
+#include <iostream.h>
 
 int main(void) {
     CTetrisBoard *board = new CTetrisBoard(10, 10);
+    for(int y=0; y<board->getHeight(); y++) {
+        for(int x=0; x<board->getWidth(); x++) {
+            cout << board->getSlot(x, y);
+            cout << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
