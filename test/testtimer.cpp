7680c1ec12acaa6139f3328b12d7fa8fac49697a
Antti Humalamäki <viagra.salesman@gmail.com>
2008-03-07 23:44:34 +0000

    timer test added

---
+++ test/testtimer.cpp
@@ -0,0 +1,19 @@
+#include "../TetrisTimer.h"
+#include <conio.h>
+#include <stdio.h>
+#include "../Thread.h"
+#include "../TetrisTimer.h"
+
+int main(void) {
+  //CTetrisTimer * ajastin;
+  CTetrisTimer::create("ajastin");
+  char ch=0;
+  printf("Timer testi, paina ESC lopettaaksesi\n");
+  while(ch != 27) {
+    if(kbhit()) {
+      ch = getch();
+      printf("ajastin on käynnissä, paina ESC lopettaaksesi\n");
+    }
+  }
+  return 0;
+}
